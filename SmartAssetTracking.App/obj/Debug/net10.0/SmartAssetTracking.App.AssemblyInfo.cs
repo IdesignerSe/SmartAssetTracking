5e7fdfafb1426f80480edbb8664f6033887d086e
idesignerSe <hebera75@gmail.com>
2026-07-24 13:17:12 +0200

    update

--- SmartAssetTracking.App/obj/Debug/net10.0/SmartAssetTracking.App.AssemblyInfo.cs
+++ SmartAssetTracking.App/obj/Debug/net10.0/SmartAssetTracking.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartAssetTracking.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ae6ecfbd0d0153ede3f317bafae7768a5c423f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9074193423051b6f727dfd0c5e358ae9cb6cc2ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartAssetTracking.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartAssetTracking.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
