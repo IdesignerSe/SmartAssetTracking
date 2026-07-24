2a11440728acae3d68e24fff3fbe53885e487300
idesignerSe <hebera75@gmail.com>
2026-07-24 13:40:12 +0200

    level 4 färdig

--- SmartAssetTracking.App/obj/Debug/net10.0/SmartAssetTracking.App.AssemblyInfo.cs
+++ SmartAssetTracking.App/obj/Debug/net10.0/SmartAssetTracking.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartAssetTracking.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7fdfafb1426f80480edbb8664f6033887d086e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5635ab9e5d47f75087fb6c80adba4abde090e6a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartAssetTracking.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartAssetTracking.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
